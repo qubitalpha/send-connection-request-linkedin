b5de705a761abd788ef426334ce8a5c74d9d4e02
--global <sendmailtochanna@gmail.com>
2019-05-29 10:08:55 -0700

    add keywords and stopwords to lookup dict

--- lookup_dict.py
+++ lookup_dict.py
@@ -8,7 +8,14 @@ Please kindly have a look into my \
 skills and I hope even you feel the \
 same.",
 'url' : "https://www.linkedin.com/company/adobe/people/?keywords=hiring",
-'file' : './connected_people/adobe.txt'}, 
+'file' : './connected_people/adobe.txt', 
+'keywords_in_status_list' : ['hiring', 'recruiting', \
+                                    'technical recruiter', \
+                                    'data engineering manager', 'talent acquisition'], 
+'stopwords_in_status_list' : ['hiring product manager', \
+                                    'hiring product design', 'design hiring', \
+                                    'hiring android', 'hiring specialist', \
+                                    'business']}, 
 
 'uber': {'my_message' : ", I'm connecting to you because \
 I found a role at Uber (SWE - Data) \
@@ -20,7 +27,14 @@ Please kindly have a look into my \
 skills and I hope even you feel the \
 same.",
 'url' : "https://www.linkedin.com/company/uber-com/people/?keywords=hiring",
-'file' : './connected_people/uber.txt'}, 
+'file' : './connected_people/uber.txt', 
+'keywords_in_status_list' : ['hiring', 'recruiting', \
+                                    'technical recruiter', \
+                                    'data engineering manager', 'talent acquisition'], 
+'stopwords_in_status_list' : ['hiring product manager', \
+                                    'hiring product design', 'design hiring', \
+                                    'hiring android', 'hiring specialist', \
+                                    'business']}, 
               
 'splunk': {'my_message' : ", I'm connecting to you to know \
 if you are hiring someone with Data Engineering\
@@ -32,7 +46,14 @@ Kindly have a look into my \
 skills and I hope even you feel the \
 same.",
 'url' : "https://www.linkedin.com/company/splunk/people/?keywords=hiring",
-'file' : './connected_people/splunk.txt'}, 
+'file' : './connected_people/splunk.txt', 
+'keywords_in_status_list' : ['hiring', 'recruiting', \
+                                    'technical recruiter', \
+                                    'data engineering manager', 'talent acquisition'], 
+'stopwords_in_status_list' : ['hiring product manager', \
+                                    'hiring product design', 'design hiring', \
+                                    'hiring android', 'hiring specialist', \
+                                    'business']}, 
 
 'apple': {'my_message' : ", I'm connecting to you to know \
 if you are hiring someone at apple with Data Engineering\
@@ -44,7 +65,14 @@ Kindly have a look into my \
 skills and I hope even you feel the \
 same.",
 'url' : "https://www.linkedin.com/company/apple/people/?facetGeoRegion=us%3A0&keywords=hiring",
-'file' : './connected_people/apple.txt'}, 
+'file' : './connected_people/apple.txt', 
+'keywords_in_status_list' : ['hiring', 'recruiting', \
+                                    'technical recruiter', \
+                                    'data engineering manager', 'talent acquisition'], 
+'stopwords_in_status_list' : ['hiring product manager', \
+                                    'hiring product design', 'design hiring', \
+                                    'hiring android', 'hiring specialist', \
+                                    'business']}, 
 
 'ericsson': {'my_message' : ", I'm connecting to you to know \
 if you are hiring someone at Ericsson with Data Engineering\
@@ -56,5 +84,12 @@ Kindly have a look into my \
 skills and I hope even you feel the \
 same.",
 'url' : "https://www.linkedin.com/company/ericsson/people/?facetGeoRegion=us%3A0&keywords=hiring",
-'file' : './connected_people/ericsson.txt'}
+'file' : './connected_people/ericsson.txt', 
+'keywords_in_status_list' : ['hiring', 'recruiting', \
+                                    'technical recruiter', \
+                                    'data engineering manager', 'talent acquisition'], 
+'stopwords_in_status_list' : ['hiring product manager', \
+                                    'hiring product design', 'design hiring', \
+                                    'hiring android', 'hiring specialist', \
+                                    'business']}
 }
